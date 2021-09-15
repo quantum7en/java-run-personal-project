165ceafbd4cb3efbd714b78a3d082f6d4ea2c782
quantum7en <altana.sarangova@gmail.com>
2021-09-15 22:08:38 +0300

    Server is enabled to start, connection is open #1

--- app/src/server/Server.java
+++ app/src/server/Server.java
@@ -5,8 +5,14 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Executor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,7 +28,7 @@ public class Server extends HttpServer {
         if(httpServerInstance == null){
             httpServerInstance = new Server() {
             };
-            logger.info("one and only server created");
+            logger.info("One and only server created.");
         }
         return httpServerInstance;
     }
@@ -30,11 +36,11 @@ public class Server extends HttpServer {
 
     @Override
     public void bind(InetSocketAddress inetSocketAddress, int i) throws IOException {
-        double x = Math.random();
+        double x = Math.random(); //for debug
 
         logger.log(Level.INFO, "binding");
         //todo figure out how to implement bind, поправить: при эксепшене здесь все равно запускается Бизнес логика и сообщение
-        if(x > 0.9)
+        if(x > 0.9) //for debug
             throw new IOException();
     }
 
@@ -42,7 +48,45 @@ public class Server extends HttpServer {
     public void start() {
         try {
             httpServerInstance = Server.getHttpServerInstance();
-            httpServerInstance.bind(new InetSocketAddress(8500), 0);
+            httpServerInstance.bind(new InetSocketAddress(8080), 0); //todo remove this
+
+            try (ServerSocket serverSocket = new ServerSocket(8080)) {
+                logger.log(Level.INFO, "Server started!");
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    logger.log(Level.INFO, "Client connected!");
+
+                    // для подключившегося клиента открываем потоки
+                    // чтения и записи
+                    try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                         PrintWriter output = new PrintWriter(socket.getOutputStream())) {
+
+                        // ждем первой строки запроса
+                        while (!input.ready()) ;
+
+                        // считываем и печатаем все что было отправлено клиентом
+                        logger.log(Level.INFO, "\n");
+                        while (input.ready()) {
+                            logger.log(Level.INFO, input.readLine());
+                        }
+
+                        // отправляем ответ
+                        output.println("HTTP/1.1 200 OK");
+                        output.println("Content-Type: text/html; charset=utf-8");
+                        output.println();
+                        output.println("<p>Masya!</p>");
+                        output.flush();
+
+                        // по окончанию выполнения блока try-with-resources потоки,
+                        // а вместе с ними и соединение будут закрыты
+                        logger.log(Level.INFO,"Client disconnected!");
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
         } catch (IOException e) {
             logger.log(Level.WARNING, "BINDING WARNING: Binding failed");
             // todo server stop
