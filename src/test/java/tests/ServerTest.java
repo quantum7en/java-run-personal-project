e11c579ae4259f1e26c52309cc5c49d36e95e678
quantum7en <altana.sarangova@gmail.com>
2021-10-05 00:14:27 +0300

    Add junit

---
+++ src/test/java/tests/ServerTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class ServerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Test
+    public static void main(String[] args) {
+
+    }
+    public void tearDown() throws Exception {
+    }
+}
